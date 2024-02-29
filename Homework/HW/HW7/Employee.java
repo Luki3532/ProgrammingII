abe17098b4eb6334e2413c766c99feb0d8c1429f
Lucas Carpenter <73721150+Luki3532@users.noreply.github.com>
2024-02-29 16:58:50 -0500

    Update Employee.java

--- Homework/HW/HW7/Employee.java
+++ Homework/HW/HW7/Employee.java
@@ -12,23 +12,28 @@ import java.util.Scanner;
 
 public class Employee {
     
-    //Attributes
-    /*
-    * First, Last name strings
-    * yearly Salary double 
-    */
+    //Attributes:
+    // First, Last name strings
+    // yearly Salary double 
     private String firstName;    
     private String lastName;
     private double monthlySalary;
     private Scanner read = new Scanner(System.in);
 
-//Constructors IF MONTHLY SALARY IS <0 THEN Monthly Salary is 0
+//Constructors:
+//  IF MONTHLY SALARY IS <0 THEN Monthly Salary is 0 because 
+//  you do not pay your job to work for them.
 public Employee(String firstName, String lastName, double monthlySalary) {
+    // DESCRIPTION: This is constructor 1, it takes all 3 attributes from the actual 
+    //  parameters and sets the class attrubutes
     this.firstName = firstName;
     this.lastName = lastName;
     this.monthlySalary = (monthlySalary < 0) ? 0 : monthlySalary;
 }
 public Employee(String firstName, String lastName){
+    // DESCRIPTION: This is constructor 2, it takes 2 atrributes(first and last name) 
+    //  and prompts the user for a salary attribute. This constructor does this with 
+    //  good use of the Scanner object
     this.firstName = firstName;
     this.lastName = lastName;
     
@@ -40,6 +45,7 @@ public Employee(String firstName, String lastName){
                      " with monthly salary attribute--> ");
     
     monthlySalary = read.nextDouble();                         // read next double from user
+    
     monthlySalary = (monthlySalary < 0) ? 0 : monthlySalary;   // check to see if monthly 
                                                                // salary is less than 0, 
                                                                // if it is monthly salary = 0 
@@ -49,7 +55,9 @@ public Employee(String firstName, String lastName){
                                                                // prepare to read again.
 }
 
-    //Getter/Accessor for first name, last name, and monthly salary.
+    //Getter/Accessor: these methods get object attributes and return them 
+    //  as either type String or double
+    //  first name, last name, and monthly salary.
     public String getFirstName() {                        
         return firstName;
     }
@@ -60,7 +68,8 @@ public Employee(String firstName, String lastName){
         return monthlySalary;
     }
 
-    //Setter/mutator for first name, last name, and monthly salary.
+    //Setter/mutator: these methods set object attributes to the given parameters
+    //  first name, last name, and monthly salary.
     public void setFirstName(String newFirstName) {
         firstName = newFirstName;
     }
@@ -71,7 +80,8 @@ public Employee(String firstName, String lastName){
         monthlySalary = newMonthlySalary;
     }
 
-    // to String mehtod
+    // to String mehtod This method returns a String that formats this objects atrributes 
+    // for a comprehensive review of the object to be read.
     public String toString() {
         return "Employee: "+                                     // Print employee name.
             firstName +" "+ 
