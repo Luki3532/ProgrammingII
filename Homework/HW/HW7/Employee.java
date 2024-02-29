f06330bf243ba7407fb353aa781b4cec7796c8a7
Lucas Carpenter <73721150+Luki3532@users.noreply.github.com>
2024-02-29 16:26:08 -0500

    Update Employee.java

--- Homework/HW/HW7/Employee.java
+++ Homework/HW/HW7/Employee.java
@@ -3,23 +3,26 @@
  * 2/28/2024
  * C201
  * Assignment #7
- * GENNERAL DESCRIPTION:
+ * GENNERAL DESCRIPTION: This class follows a Employee with first, last names,
+ * and monthly salary. This class hosts getters, setters, and a toString method.
+ * this class has 2 constructors: the first sets all three attributes and the
+ * other prompts 
  */
 import java.util.Scanner;
 
 public class Employee {
     
     //Attributes
-        /*
-         * First, Last name strings
-         * yearly Salary double
-         */
-    private String firstName;
+    /*
+    * First, Last name strings
+    * yearly Salary double 
+    */
+    private String firstName;    
     private String lastName;
     private double monthlySalary;
     private Scanner read = new Scanner(System.in);
 
-    //Constructor
+//Constructors IF MONTHLY SALARY IS <0 THEN Monthly Salary is 0
 public Employee(String firstName, String lastName, double monthlySalary) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -29,16 +32,25 @@ public Employee(String firstName, String lastName){
     this.firstName = firstName;
     this.lastName = lastName;
     
-    //Prompt user to enter a monthly salary, must be a positive number. if negative slary then pass 0 to class attribute 
-    System.out.print("Please provide employee "+firstName+" "+lastName+" with monthly salary attribute--> ");
-    monthlySalary = read.nextDouble();
-    monthlySalary = (monthlySalary < 0) ? 0 : monthlySalary;
+    //Prompt user to enter a monthly salary, must be a positive number. 
+    // if negative slary then pass 0 to class attribute 
+    System.out.print("Please provide employee "+
+                     firstName+" "+
+                     lastName+
+                     " with monthly salary attribute--> ");
+    
+    monthlySalary = read.nextDouble();                         // read next double from user
+    monthlySalary = (monthlySalary < 0) ? 0 : monthlySalary;   // check to see if monthly 
+                                                               // salary is less than 0, 
+                                                               // if it is monthly salary = 0 
+                                                               // otherwise it stays the same
 
-    read.nextLine();
+    read.nextLine();                                           // leave the current line,
+                                                               // prepare to read again.
 }
 
-    //Getter/Accessor
-    public String getFirstName() {
+    //Getter/Accessor for first name, last name, and monthly salary.
+    public String getFirstName() {                        
         return firstName;
     }
     public String getLastName() {
@@ -48,7 +60,7 @@ public Employee(String firstName, String lastName){
         return monthlySalary;
     }
 
-    //Setter/mutator
+    //Setter/mutator for first name, last name, and monthly salary.
     public void setFirstName(String newFirstName) {
         firstName = newFirstName;
     }
@@ -61,6 +73,9 @@ public Employee(String firstName, String lastName){
 
     // to String mehtod
     public String toString() {
-        return "Employee: "+ firstName +" "+ lastName+", has a monthly salary of: $"+ monthlySalary;
+        return "Employee: "+                                     // Print employee name.
+            firstName +" "+ 
+            lastName+ ", has a monthly salary of: $"+ 
+            monthlySalary;                                       // print monthly salary.
     }
 }
